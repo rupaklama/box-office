f19a26f43206342cc3f6c8b58ac2d39c010d65d8
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2020-10-21 20:43:14 -0700

    Add files via upload

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,78 +1,33 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-import tvMazeApi from '../api/tvMaze';
-import ShowMainData from '../components/show/ShowMainData';
-import Details from '../components/show/Details';
-import Seasons from '../components/show/Seasons';
-import Cast from '../components/show/Cast';
-import { ShowPageWrapper, InfoBlock } from './Show.styled';
-
 const Show = () => {
-  // const [results, setResults] = useState(null);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // useParams hook matches current url param
   const { id } = useParams();
-  // console.log('params', id)
+  // console.log('params', params)
 
-  const initialState = {
-    results: null,
-    isLoading: true,
-    error: null,
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'fetch_success':
-        return {
-          // ...state,
-          isLoading: false,
-          results: action.payload,
-          error: null,
-        };
-      case 'fetch_failed': 
-        return {
-          ...state,
-          isLoading: false,
-          error: action.error,
-        }
-      default:
-        return state;
-    }
-  };
-
-  // objects destructuring
-  const [{ results, isLoading, error }, dispatch] = useReducer(reducer, initialState);
-  console.log(results)
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     let isMounted = true;
-    const fetch = async () => {
-      try {
-        const { data } = await tvMazeApi.get(
-          `/shows/${id}?embed[]=seasons&embed[]=cast`
-        );
 
-        if (isMounted) {
-          // setResults(data);
-          // setIsLoading(false);
-          dispatch({ type: 'fetch_success', payload: data })
-        }
-      } catch (err) {
-        if (isMounted) {
-          // setError(err.message);
-          // setIsLoading(false);
-          dispatch({ type: 'fetch_failed', payload: err.message })
-        }
-      }
-    };
+    setIsLoading(true);
 
-    fetch();
-    // this clean up func gets executed before calling on next callback func
-    // or componentDidMount, doing clean up before starting something new
-    // which prevents memory leak - unnecessary use of RAM & app performance degrades
+    fetch(`https://api.tvmaze.com/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(response => response.json())
+      .then(data => {
+        if (isMounted) {
+          setShow(data);
+          setIsLoading(false);
+        }
+      })
+      .catch(error => {
+        if (isMounted) {
+          setError(error.message);
+          setIsLoading(false);
+        }
+      });
+
     return () => {
       isMounted = false;
     };
@@ -86,36 +41,7 @@ const Show = () => {
     return <div>Error occurred: {error}</div>;
   }
 
-  return (
-    <ShowPageWrapper>
-      <ShowMainData
-        image={results.image}
-        name={results.name}
-        rating={results.rating}
-        summary={results.summary}
-        tags={results.genres}
-      />
-
-      <InfoBlock>
-        <h2>Details</h2>
-        <Details 
-          status={results.status} 
-          network={results.network} 
-          premiered={results.premiered}
-        />
-      </InfoBlock>
-
-      <InfoBlock>
-        <h2>Seasons</h2>
-        <Seasons seasons={results._embedded.seasons} />
-      </InfoBlock>
-
-      <InfoBlock>
-        <h2>Casts</h2>
-        <Cast cast={results._embedded.cast}/>
-      </InfoBlock>
-    </ShowPageWrapper>
-  );
+  return <div>show page</div>;
 };
 
 export default Show;
